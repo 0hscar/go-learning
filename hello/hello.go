6106c474dcb75801deaf42dfba3526d81cb058ce
Oscar <oscar.weber00@gmail.com>
2024-05-04 15:04:37 +0300

    Final commit

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Glady")
-	fmt.Println(message)
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Pär", "Lars", "Tjong"}
+
+	messages, err := greetings.Hellos(names) //Does not return error, if "" returns error
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
